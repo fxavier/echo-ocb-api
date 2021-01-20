b1810b8f1e6f8f93b396e641f7118ac2d5b7becb
Xavier Francisco <xavierfrancisco353@gmail.com>
2021-01-20 19:33:06 +0200

    Initialize

---
+++ app/core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from core import models
+
+admin.site.register(models.Provincia)
+admin.site.register(models.Distrito)
+admin.site.register(models.UnidadeSanitaria)
+admin.site.register(models.ResumoMensalVSL)
+admin.site.register(models.ProgramaRadio)
+admin.site.register(models.ActaMensalCG)
+admin.site.register(models.DialogoComunitario)
---
+++ app/core/models.py
@@ -0,0 +1,124 @@
+from django.db import models
+
+
+class Provincia(models.Model):
+    nome = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome
+
+
+class Distrito(models.Model):
+    nome = models.CharField(max_length=150)
+    provincia = models.ForeignKey(Provincia, on_delete=models.CASCADE)
+
+    def __str(self):
+        return self.nome
+
+
+class UnidadeSanitaria(models.Model):
+    nome = models.CharField(max_length=150)
+    distrito = models.ForeignKey(Distrito, on_delete=models.CASCADE)
+
+    def __str(self):
+        return self.nome
+
+
+class ActaMensalCG(models.Model):
+    unidade_sanitaria = models.ForeignKey(
+        UnidadeSanitaria, on_delete=models.CASCADE)
+    data_encontro = models.DateField(auto_now=False, auto_now_add=False)
+    data_proximo_encontro = models.DateField(
+        auto_now=False, auto_now_add=False)
+    num_homens = models.IntegerField()
+    num_mulheres = models.IntegerField()
+    nome_pessoa_facilitador = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome_pessoa_facilitador + " " + self.data_encontro
+
+
+class ResumoMensalVSL(models.Model):
+    unidade_sanitaria = models.ForeignKey(
+        UnidadeSanitaria, on_delete=models.CASCADE)
+    nome_pessoa_facilitador = models.CharField(max_length=150)
+    data_inicial = models.DateField(auto_now=False, auto_now_add=False)
+    data_final = models.DateField(auto_now=False, auto_now_add=False)
+    total_grupos = models.IntegerField()
+    num_novos_mes = models.IntegerField()
+    num_grupos_encerram_ciclo = models.IntegerField()
+    membros_feminino = models.IntegerField()
+    membros_masculino = models.IntegerField()
+    membros_feminino_HIV_Pos = models.IntegerField()
+    membros_masculino_HIV_Pos = models.IntegerField()
+    perc_membros_feminino_HIV_Pos = models.DecimalField(
+        max_digits=10, decimal_places=2)
+    perc_membros_masculino_HIV_Pos = models.DecimalField(
+        max_digits=10, decimal_places=2)
+    cuid_cov_feminino = models.IntegerField()
+    cuid_cov_masculino = models.IntegerField()
+    membros_gaac_feminino = models.IntegerField()
+    membros_gaac_masculino = models.IntegerField()
+    outros_membros_feminino = models.IntegerField()
+    outros_membros_masculino = models.IntegerField()
+    membros_cs_feminino = models.IntegerField()
+    membros_cs_masculino = models.IntegerField()
+    membros_maes_feminino = models.IntegerField()
+    membros_maes_masculino = models.IntegerField()
+
+    def __str__(self):
+        return self.nome_pessoa_facilitador + " " + self.data_inicial
+
+
+class DialogoComunitario(models.Model):
+    unidade_sanitaria = models.ForeignKey(
+        UnidadeSanitaria, on_delete=models.CASCADE)
+    nome_facilitador = models.CharField(max_length=150)
+    data_inicial = models.DateField(auto_now=False, auto_now_add=False)
+    data_final = models.DateField(auto_now=False, auto_now_add=False)
+    total_grupos = models.IntegerField()
+    grupos_novos_mes = models.IntegerField()
+    grupos_encerram_ciclo = models.IntegerField()
+    participantes_fem_20_24 = models.IntegerField()
+    participantes_fem_20_24 = models.IntegerField()
+    participantes_fem_25_plus = models.IntegerField()
+    participantes_fem_15_19 = models.IntegerField()
+    participantes_masc_20_24 = models.IntegerField()
+    participantes_masc_25_plus = models.IntegerField()
+    participantes_masc_20_24 = models.IntegerField()
+    participantes_masc_20_24 = models.IntegerField()
+    participantes_masc_25_plus = models.IntegerField()
+    participantes_masc_15_19 = models.IntegerField()
+    referido = models.IntegerField()
+    testagem = models.IntegerField()
+    outros_servicos = models.IntegerField()
+
+    def __str(self):
+        return self.nome_facilitador + " " + self.data_inicial
+
+
+class ProgramaRadio(models.Model):
+    supervisor_cbo = models.CharField(max_length=150)
+    nome_radio_comunitaria = models.CharField(max_length=150)
+    data_inicio = models.DateField(auto_now=False, auto_now_add=False)
+    data_final = models.DateField(auto_now=False, auto_now_add=False)
+    ret_geral_spots = models.IntegerField()
+    ret_geral_debates = models.IntegerField()
+    ret_geral_obs = models.IntegerField()
+    ret_gravida_spots = models.IntegerField()
+    ret_gravida_debates = models.IntegerField()
+    ret_gravida_obs = models.IntegerField()
+    tb_hiv_spots = models.IntegerField()
+    tb_hiv_debates = models.IntegerField()
+    tb_hiv_obs = models.IntegerField()
+    tarv_crianca_spots = models.IntegerField()
+    tarv_crianca_debates = models.IntegerField()
+    tarv_crianca_obs = models.IntegerField()
+    gbv_spots = models.IntegerField()
+    gbv_debates = models.IntegerField()
+    literacia_tarv_spot = models.IntegerField()
+    literacia_tarv_database = models.IntegerField()
+    literacia_tarv_obs = models.IntegerField()
+
+    def __str__(self):
+        return self.supervisor_cbo + " " + self.data_inicio
